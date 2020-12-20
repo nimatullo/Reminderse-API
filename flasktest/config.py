2ca5f1edbc545c266cb1f96871793173e83fec3c
shermanjones99@gmail.com <dan.abramov@gmail.com>
2020-12-20 10:12:47 -0500

    added database-url

--- flasktest/config.py
+++ flasktest/config.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     MAIL_SERVER = 'smtp.zoho.com'
     MAIL_PORT = 465
     MAIL_USE_TLS = True
