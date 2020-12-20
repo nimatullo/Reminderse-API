16c7d089e95f4796594dfbaf529623d667b41729
shermanjones99@gmail.com <dan.abramov@gmail.com>
2020-12-20 10:49:50 -0500

    doing it my own way 😈

--- commands.py
+++ commands.py
@@ -1,10 +1,4 @@
-import click
-from flask.cli import with_appcontext
-
 from flasktest import db
-from models import Users, Links, Text, Category
+from flasktest.models import Users, Links, Text, Category
 
-@click.command(name="create_tables")
-@with_appcontext
-def create_tables():
-    db.create_all()
+db.create_all()
