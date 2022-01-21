7774a2f3a0aa98797b841b2b48e7e82556ebe8c2
Sherzod <shermanjones99@gmail.com>
2022-01-20 20:48:16 -0500

    updated cors

--- flasktest/__init__.py
+++ flasktest/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask.json import jsonify
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
@@ -6,6 +7,8 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 from itsdangerous import URLSafeTimedSerializer
+from flask_swagger_ui import get_swaggerui_blueprint
+from flask_swagger import swagger
 
 app = Flask(__name__)
 # CORS(app, resources={r'/*': {"origins": ["https://localhost:3000",
@@ -13,7 +16,7 @@ app = Flask(__name__)
 # CORS(app, resources={r'/api/*': {
 #     "origins":  ["https://618ec617dcd64c000799a047--vigorous-varahamihira-d00b87.netlify.app"]
 #     }}, supports_credentials=True)
-cors = CORS(app, origins=["https://618ec617dcd64c000799a047--vigorous-varahamihira-d00b87.netlify.app",
+cors = CORS(app, origins=["https://deploy-preview-6--vigorous-varahamihira-d00b87.netlify.app",
                           "https://www.reminderse.com"
                           ],
             headers=['Content-Type'],
@@ -36,7 +39,6 @@ mail = Mail(app)
 login_manager.session_protection = 'none'
 ts = URLSafeTimedSerializer(app.config["SECRET_KEY"])
 jwt = JWTManager(app)
-
 print(app.config)
 
 version = "0.0.1"
@@ -46,3 +48,10 @@ from flasktest.entries.routes import entries
 
 app.register_blueprint(users)
 app.register_blueprint(entries)
+app.register_blueprint(get_swaggerui_blueprint(
+    app.config["SWAGGER_URL"],
+    app.config["API_URL"],
+    config={
+        'app_name': 'Reminderse API'
+    }
+))
