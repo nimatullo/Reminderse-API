b29e36664334589f2e837b27272abeb04d2ddc77
Sherzod <shermanjones99@gmail.com>
2022-05-14 11:05:55 -0400

    default value for sql url

--- core/database/database.py
+++ core/database/database.py
@@ -3,17 +3,21 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from decouple import config
 
-SQL_ALCHEMY_DATABASE_URL = config("SQL_URL")
+SQL_ALCHEMY_DATABASE_URL = (
+    config("SQL_URL") if config("SQL_URL") else "sqlite:///db.sqlite3"
+)
 
 engine = create_engine(SQL_ALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 
 def get_db():
     try:
         db = SessionLocal()
         yield db
     finally:
-      db.close()    
+        db.close()
+
 
 Base = declarative_base()
